e708bb0504f1b40408fd2c76b9a6c592e1221f69
AKSHAYKUMAR2862 <2311cs020075@mallareddyuniversity.ac.in>
2025-01-07 20:44:46 +0530

    Add files via upload

---
+++ Day-3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Taking input for marks in three subjects
+subject_1 = float(input("Enter marks for subject 1: "))
+subject_2 = float(input("Enter marks for subject 2: "))
+subject_3 = float(input("Enter marks for subject 3: "))
+average_marks = (subject_1 + subject_2 + subject_3) / 3
+if average_marks >= 90:
+    print("Grade: A")
+elif 80 <= average_marks < 90:
+    print("Grade: B")
+elif 70 <= average_marks < 80:
+    print("Grade: C")
+else:
+    print("Grade: Fail")
+
+
+# In[ ]:
+
+
+
+
